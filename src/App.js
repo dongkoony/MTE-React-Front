c1fefc30922c0b6485b575679a8c1191f46058a6
donghyeon shin <109497684+dongkoony@users.noreply.github.com>
2023-01-02 21:35:10 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./style.css";
 import TextField from '@mui/material/TextField';
 import Checkbox from '@mui/material/Checkbox';
 import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 export default function App() {
   return (
     <div>
-      <TextField label="login ID" required fullWidth/>
-      <TextField label="login Password" type="password" required fullWidth/>
-      <TextField label="IP Address" required fullWidth/>
-      <TextField label="Port"  required fullWidth/>
-      <Checkbox value="remember" color="primary" />
-      <Button type="submit">submission</Button>
+      <TextField label="login ID" required fullWidth name="login ID" autoComplete="login ID" autoFocus sx={{mb:3}} />
+      <TextField label="login Password" type="password" required fullWidth name="password" autoComplete="password" sx={{mb:3}} />
+      <TextField label="IP Address" required fullWidth name="ip" sx={{mb:3}} />
+      <TextField label="Port"  required fullWidth name="port" sx={{mb:3}} />
+      <FormControlLabel control={<Checkbox value="remember" color="primary" />} label="Remember me" />
+      
+      <Button type="submit" fullWidth variant="contained" sx={{mt:3}}>submission</Button>
     </div>
-  );
+  ); 
 }
